4a4287a6645965fdac5ec9c0c852067f812f8f1e
tikangzhang <zhangtikang778899@163.com>
2020-03-24 19:04:58 +0800

    add new

--- src/test/java/TestDwg2Svg.java
+++ src/test/java/TestDwg2Svg.java
@@ -2,29 +2,30 @@ import com.aspose.cad.Image;
 import com.aspose.cad.fileformats.cad.CadDrawTypeMode;
 import com.aspose.cad.imageoptions.CadRasterizationOptions;
 import com.aspose.cad.imageoptions.PngOptions;
+import com.aspose.cad.imageoptions.SvgOptions;
 import com.aspose.cad.imageoptions.UnitType;
 
-public class TestDwg2Png {
+public class TestDwg2Svg {
     public static void main(String[] args) throws Exception {
         String sourcePath = "D:\\IDEA\\cad\\17.dwg";
-        String targetPath = "D:\\IDEA\\cad\\17.png";
+        String targetPath = "D:\\IDEA\\cad\\17.svg";
 
-        PngOptions pngOptions = new PngOptions();
-        pngOptions.setCompressionLevel(0);
+        SvgOptions options = new SvgOptions();
         try {
             Image image = Image.load(sourcePath);
-            int unitType = image.getUnitType();
             int width = image.getWidth();
             int height = image.getHeight();
             int zoom = 1008000 / width / height;//100万像素等比倍数
+
             CadRasterizationOptions cadRasterizationOptions = new CadRasterizationOptions();
             cadRasterizationOptions.setPageHeight(height * zoom);
             cadRasterizationOptions.setPageWidth(width * zoom);
             cadRasterizationOptions.setDrawType(CadDrawTypeMode.UseObjectColor);
-            cadRasterizationOptions.setUnitType(UnitType.Unitless);
 
-            pngOptions.setVectorRasterizationOptions(cadRasterizationOptions);
-            image.save(targetPath, pngOptions);
+            options.setVectorRasterizationOptions(cadRasterizationOptions);
+            //options.setColorType(SvgColorMode.Grayscale);
+            //options.setTextAsShapes(true);
+            image.save(targetPath,options);
         }catch (Exception ex){
 
         }
